0e28a8e4a821d850b27024e04b43600fc5dba31d
jeremyrice98 <ricefamily1003@gmail.com>
2020-08-08 11:40:57 -0600

    add json files

---
+++ server.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const express = require('express');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const session = require('express-session');
+const helpers = require('./utils/helpers');
+
+// PORT
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// MIDDLEWARE
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// HANDLEBARS
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({ helpers });
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars')
+
+// SET UP SESSION
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sess = {
+  secret: process.env.SECRET,
+  cookie: {
+      maxAge: 30 * 60000,
+      sameSite: 'strict'
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+      db: sequelize
+  })
+};
+app.use(session(sess));
+
+// ROUTES
+app.use(routes);
+
+// CONNECTION
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => 
+  console.log(`API server now on port ${PORT}!`));
+});
